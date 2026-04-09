dabfb414e9a25563bbfb2243e874542d87901aaf
albertestop <albert.estop.cepero@gmail.com>
2026-04-09 15:35:16 +0200

    perceiver model added

--- src/v1tovideo/neural_autoencoder/models/__init__.py
+++ src/v1tovideo/neural_autoencoder/models/__init__.py
@@ -6,10 +6,13 @@ from .core import (
 )
 from .loading import build_model_from_target
 from .TAE import TAE
+from .PAE import PAE
 
 __all__ = [
     "BaseNeuralAutoencoder",
     "MLPNeuralAutoencoder",
+    "PerceiverAE",
+    "TAE",
     "TemplateNeuralAutoencoder",
     "TransformerNeuralAutoencoder",
     "build_model",
